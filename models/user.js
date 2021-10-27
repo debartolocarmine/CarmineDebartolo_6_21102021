e051707088c691dc833503b765ade716c71ec93f
debartolocarmine <debartolocarmine@gmail.com>
2021-10-27 13:15:49 +0200

    cd-002-Adding data model for users

---
+++ models/user.js
@@ -0,0 +1,15 @@
+// importation de mongoose
+const mongoose = require('mongoose');
+// installation d'un package de validation pour pré-valider les informations avant de les enregistrer.
+const uniqueValidator = require('mongoose-unique-validator');
+
+const userSchema = mongoose.Schema({
+  /* Dans notre schéma, la valeur unique , avec l'élément mongoose-unique-validator passé comme plug-in,
+  s'assurera qu'aucun des deux utilisateurs ne peut partager la même adresse e-mail.*/
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+});
+
+userSchema.plugin(uniqueValidator);
+
+module.exports = mongoose.model('User', userSchema);
